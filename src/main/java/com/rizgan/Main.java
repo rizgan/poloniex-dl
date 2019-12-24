85f657a5b20c256ed517a0d1ec7e98aa09c1be38
Rizgan <rizgan@gmail.com>
2019-12-24 23:50:27 +0300

    Working

--- src/main/java/com/rizgan/Main.java
+++ src/main/java/com/rizgan/Main.java
@@ -6,8 +6,33 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
 
-        CoinPair.coinPairDownloder();
+    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
+        startFetchingData();
+    }
+
+
+    public static void startFetchingData() throws InterruptedException {
+        try {
+            for (int i = 5; i < 10; i++) {
+                CoinPair.coinPairDownloder(i);
+            }
+        } catch (Exception e) {
+            Thread.sleep(2000);
+            for (int i = 5; i < 10; i++) {
+                try {
+                    CoinPair.coinPairDownloder(i);
+                } catch (IOException e1) {
+                    Thread.sleep(2000);
+                    startFetchingData();
+                    e1.printStackTrace();
+                } catch (ParseException e1) {
+                    Thread.sleep(2000);
+                    e1.printStackTrace();
+                    startFetchingData();
+
+                }
+            }
+        }
     }
 }
